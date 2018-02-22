5709983ebe5c64576ae407e59640e1ff8b919c56
Janice <Guest@Epicodus-7E.local>
2018-02-21 19:17:27 -0800

    add modified html and images

--- js/scripts.js
+++ js/scripts.js
@@ -6,24 +6,24 @@
 $(document).ready(function() {
 
   $("form#quiz").submit(function(event) {
-      var seasons = $("#seasons").val();
+    event.preventDefault();
+    var seasons = $("#seasons").val();
+    var nameInput = $("input#name").val();
+
+    if (seasons === 1) {
+      $("#capetown").show();
 
       // var question1 = ($("#question1").val());
       // var question2 = ($("#question2").val());
       // var question3 = ($("#question3").val());
       // var question4 = ($("#question4").val());
       // var question5 = ($("#question5").val());
-      var nameInput = $("input#firstname").val();
 
-      $("#name").text(nameInput);
-
-      // if (seasons === 1) {
-      //   $("#capetown").show();
 
 
       // } else if (seasons === 2) {
       //   $("#borabora").show();
 
-      event.preventDefault();
+    }
   })
 })
