e65db1a29d0b56357e2e7b71d647256af11770aa
Janice <janicejoonam@gmail.com>
2018-02-23 14:58:34 -0800

    add all the modifications to the css html and js files, defined var for each survey question and added div id classification for each question

--- js/scripts.js
+++ js/scripts.js
@@ -5,27 +5,70 @@
 
 $(document).ready(function() {
 
-  $("form#quiz").submit(function(event) {
+  $("#quiz form").submit(function(event) {
     event.preventDefault();
-      var seasons = $("#seasons").val();
+
+    var name = $("input#name").val();
+    var seasons = $("select#seasons").val();
+    // var location = $("div#locations").val();
+    // var activities = $("div#activities").val();
+    // var preference = $("div#preference").val();
+    // var importance = $("div#importance").val();
+    if (seasons) {
+      if (seasons === "spring") {
+        $("#capetown").show();
+        $("#borabora").hide();
+        $("#montreal").hide();
+        $("#erfurt").hide();
+      } else if (seasons === "summer") {
+        $("#capetown").hide();
+        $("#borabora").show();
+        $("#montreal").hide();
+        $("#erfurt").hide();
+      } else if (seasons === "fall") {
+        $("#capetown").hide();
+        $("#borabora").hide();
+        $("#montreal").show();
+        $("#erfurt").hide();
+      } else if (seasons === "winter") {
+        $("#capetown").hide();
+        $("#borabora").hide();
+        $("#montreal").hide();
+        $("#erfurt").show();
+      } else {
+        alert("Please make sure to answer all the questions!");
+        var surveyResults = $("input#name").val();
+        $(".name").append(surveyResults);
+        $("#surveyresults").show();
+
+      }
+      }
+      }
+      }
+      }
+    }
+
+
+    // $(".name").text(nameInput);
+    // $("#capetown").show();
+
+  });
+});
+
+
+    // if (seasons === 1) {
+    //   $("#capetown").show();
 
       // var question1 = ($("#question1").val());
       // var question2 = ($("#question2").val());
       // var question3 = ($("#question3").val());
       // var question4 = ($("#question4").val());
       // var question5 = ($("#question5").val());
-      var nameInput = $("input#firstname").val();
 
-      $("#name").text(nameInput);
-
-      if (#seasons === 1) {
-        $("#capetown").show();
 
 
       // } else if (seasons === 2) {
       //   $("#borabora").show();
 
-
-    }
-  });
-});
+//     }
+//   })
