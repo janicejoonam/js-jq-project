56a1ae7cc0003e4dda8644eb39036635933c8e00
Janice <janicejoonam@gmail.com>
2018-02-24 18:42:30 -0800

    add changes to css/styles.css and js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,6 @@ $(document).ready(function() {
   $("#quiz form").submit(function(event) {
     event.preventDefault();
 
-
-    var name = $("input#name").val();
     var seasons = $("select#seasons").val();
     var location = $("select#locations").val();
     var activities = $("select#activities").val();
@@ -37,25 +35,10 @@ $(document).ready(function() {
         $("#erfurt").show();
       } else {
         alert("Please make sure to answer all the questions!");
-        var surveyResults = $("input#name").val();
-        // $(".name").text(surveyResults);
-        $(".name").text(nameInput);
+        var nameInput = $("input#name").val();
+        $(".name").append(nameInput);
         $("#surveyresults").show();
+      };
 
-      }
     })
-      // }
-      // }
-      // }
-    })
-
-
-    // $(".name").text(nameInput);
-    // $("#capetown").show();
-    //
-    // var nameInput = $("input#name").val();
-    // var seasons = $("#seasons").val();
-    //
-    // $(".name").text(nameInput);
-    // // if (seasons === )
-    // $("#capetown").show();
+  });
